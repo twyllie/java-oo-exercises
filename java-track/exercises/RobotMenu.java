c1a8eb12e2dd45441617d3903dc5f092da3a97d5
Tynan <quixotichamster@gmail.com>
2016-10-04 18:19:03 -0500

    Stubbed out RobotMenu.java

--- java-track/exercises/RobotMenu.java
+++ java-track/exercises/RobotMenu.java
@@ -9,5 +9,43 @@ public class RobotMenu {
 	private Scanner s;
 	private ArrayList<Robot> robots;
 	
+	public static void main(String[] args){
+		
+	}
+	
+	public RobotMenu(){
+		s = new Scanner(System.in);
+		robots = new ArrayList<Robot>();
+	}
+	
+	public int menu(){
+		
+	}
+	
+	public void processIn(){
+		
+	}
+	
+	public void createRobot(){
+		
+	}
+	
+	public void listRobots(){
+		
+	}
+	
+	public void moveRobot(){
+		
+	}
+	
+	public void rotateRobot(){
+		
+	}
+	
+	public void distanceCheck(){
+		
+	}
+	
+	//TODO: Stub out methods as a means of designing.
 	//TODO: Once tests have been written, implement your RobotMenu.
 }
