53dd4dd5ef6eb49211253221f5aecf99e2d16b6c
Tynan <quixotichamster@gmail.com>
2016-10-04 19:07:13 -0500

    Stubbed out RobotMenuTest.

--- java-track/exercises/RobotMenuTest.java
+++ java-track/exercises/RobotMenuTest.java
@@ -5,10 +5,53 @@ import org.junit.Test;
 public class RobotMenuTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRobotMenuInit() {
+		//Test that both an array and a scanner are initialized
+	}
+	
+	@Test
+	public void testMainLoop(){
+		//Test that the main loop exits on the right value.
+		//Test that it calls the other method properly.
 	}
 
+	@Test
+	public void testMenu(){
+		//Test that the menu outputs the right things to console.
+		//Test that it gets and returns the right values.
+	}
+	
+	@Test
+	public void testProcessIn(){
+		//Test that given a value it calls the correct method.
+	}
+	
+	@Test
+	public void testCreateRobot(){
+		//Test that it calls the method correctly, with all of the right values.
+		//Test that it properly populates the Robot array.
+	}
+	
+	@Test
+	public void testListRobots(){
+		//Test that the output on console is correct.
+	}
+	
+	@Test
+	public void testMoveRobot(){
+		//Test that it calls list method properly
+		//Test that the move() method is called correctly
+	}
+	
+	@Test
+	public void testRotateRobot(){
+		//Test that it calls list method properly
+		//Test that it calls rotate() properly.
+	}
+	
+	@Test
+	public void testDistanceCheck(){
+		//Test for listings.
+		//Test for Robot method call.
+	}
 }
-
-// TODO: After designing, but before coding, write tests for the intended parameters of RobotMenu.
