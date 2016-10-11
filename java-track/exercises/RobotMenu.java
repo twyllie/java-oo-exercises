8203364d921d50a6765ab1792e2fd9379e219728
Tynan <quixotichamster@gmail.com>
2016-10-11 17:53:32 -0500

    RobotMenu is tested, fixed, and approved

--- java-track/exercises/RobotMenu.java
+++ java-track/exercises/RobotMenu.java
@@ -16,6 +16,7 @@ public class RobotMenu {
 	
 	public int menu(){
 		int choice;
+		System.out.println(" ");
 		System.out.println("-----------------------------------------");
 		System.out.println("Please select an option:");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
@@ -26,6 +27,7 @@ public class RobotMenu {
 		System.out.println("5. Compute the distance between robots.");
 		System.out.println("6. Exit.");
 		System.out.println("-----------------------------------------");
+		System.out.println(" ");
 		choice = s.nextInt();
 		return choice;
 	}
@@ -49,28 +51,36 @@ public class RobotMenu {
 			break;
 		case 6:
 			System.out.println("Why, Dave?");
+			System.out.println(" ");
+			break;
 		default:
 			System.out.println("That is an invalid selection.");
+			System.out.println(" ");
 		}
 	}
 	
 	public void createRobot(){
 		String name;
 		int x, y, speed, direction;
-		System.out.println("");
+		System.out.println(" ");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
 		System.out.println("You have selected to create a new robot.");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
 		System.out.println("Please select a name for your robot.");
-		name = s.nextLine();
-		System.out.print("Please select an x position.");
+		System.out.println(" ");
+		name = s.next();
+		System.out.println("Please select an x position.");
+		System.out.println(" ");
 		x = s.nextInt();
 		System.out.println("Please select a y position.");
+		System.out.println(" ");
 		y = s.nextInt();
 		System.out.println("Please enter a starting speed.");
+		System.out.println(" ");
 		speed = s.nextInt();
 		System.out.println("Please enter a direction");
 		System.out.println("1. NORTH\n2. EAST\n3. SOUTH\n4. WEST");
+		System.out.println(" ");
 		direction = s.nextInt();
 		switch(direction){
 			case 1: direction = 360;
@@ -89,47 +99,54 @@ public class RobotMenu {
 	public boolean listRobots(){
 		if(robots.isEmpty()){
 			System.out.println("There are no robots, please create one of my brethren.");
+			System.out.println(" ");
 			return false;
 		}else{
 			for(int i = 0; i < robots.size(); i ++){
 				System.out.println((i+1) + " " + robots.get(i));
-				return true;
-			}
-		}return false;
+			} return true;
+		}
 	}
 	
 	public void moveRobot(){
-		System.out.println("");
+		System.out.println(" ");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
 		System.out.println("You have selected to move a robot.");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
+		System.out.println(" ");
 		boolean rob;
 		rob = this.listRobots();
 		if(rob == false){
 			return;
 		}else{
 			System.out.println("Please select a bot to move.");
-			int input = s.nextInt();
+			System.out.println(" ");
+			int input = (s.nextInt()-1);
 			robots.get(input).move();
 			System.out.println("Here's where that bot's new bot spot.");
+			System.out.println(" ");
 			System.out.println(robots.get(input));
+			System.out.println(" ");
 		}
 		
 	}
 	
 	public void rotateRobot(){
-		System.out.println("");
+		System.out.println(" ");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
 		System.out.println("You have selected to rotate a robot.");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
+		System.out.println(" ");
 		boolean rob;
 		rob = this.listRobots();
 		if(rob == false){
 			return;
 		}else{
 			System.out.println("Please select a bot to rotate");
-			int input = s.nextInt();
+			System.out.println(" ");
+			int input = s.nextInt()-1;
 			System.out.println("Would you like your robot to rotate 1.LEFT or 2.RIGHT? ");
+			System.out.println(" ");
 			int direction = s.nextInt();
 			switch(direction){
 				case 1: direction = -90;
@@ -140,29 +157,35 @@ public class RobotMenu {
 			robots.get(input).rotate(direction);
 			System.out.println("The robot's new heading is:");
 			System.out.println(robots.get(input));
+			System.out.println(" ");
 		}
 	}
 	
 	public void distanceCheck(){
-		System.out.println("");
+		System.out.println(" ");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
 		System.out.println("You have selected to check the distance between two robots.");
-		System.out.print("Are you planning an attack, meat-sack?");
+		System.out.println("Are you planning an attack, meat-sack?");
 		System.out.println("- - - - - - - - - - - - - - - - - - - - -");
+		System.out.println(" ");
 		boolean rob;
 		rob = this.listRobots();
 		if(rob == false){
 			return;
 		}else if(robots.size() < 2){
 			System.out.println("More than one robot is needed to compute the distance between *TWO* robots, flesh-pile.");
+			System.out.println(" ");
 			return;
 		}else{
 			System.out.println("Select the first robot.");
-			int robot1 = s.nextInt();
+			System.out.println(" ");
+			int robot1 = s.nextInt()-1;
 			System.out.println("Slect the second robot.");
-			int robot2 = s.nextInt();
+			System.out.println(" ");
+			int robot2 = s.nextInt()-1;
 			double distance = robots.get(robot1).distanceToBot(robots.get(robot2));
 			System.out.println("The distance between your targets is " + distance);
+			System.out.println(" ");
 		}
 	}
 	
