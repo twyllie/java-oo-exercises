900da4effde0ca6657f7bef667d7c7e289d52491
Tynan <quixotichamster@gmail.com>
2016-10-19 18:07:45 -0500

    Removed unused code

--- java-track/assignments/javagram/Javagram.java
+++ java-track/assignments/javagram/Javagram.java
@@ -5,7 +5,6 @@ import javagram.filters.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Scanner;
 
 public class Javagram {
